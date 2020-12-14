7cde20a2ae5efd0b23a98cdc41ed263743761f39
ShirleyOM <shirleyondieki@gmail.com>
2020-12-14 13:05:48 -0600

    First commit

---
+++ springboot-jpa-crud-tutorial/src/main/java/com/springboot/angular/springbootjpacrudtutorial/SpringbootJpaCrudTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.angular.springbootjpacrudtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaCrudTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaCrudTutorialApplication.class, args);
+	}
+
+}
---
+++ springboot-jpa-crud-tutorial/src/test/java/com/springboot/angular/springbootjpacrudtutorial/SpringbootJpaCrudTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.angular.springbootjpacrudtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaCrudTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
